339d1dced64c404b82124ff3c3a0fc781616faad
Reza <reza.daneshvar@majority.com>
2025-11-14 17:27:45 +0100

    Initial Commit

---
+++ animation-issue-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  animation-issue-demo
+//
+//  Created by Reza on 2025-11-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
