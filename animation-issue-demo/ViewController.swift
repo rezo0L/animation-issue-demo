ec478b1044dda7b31779087c0f3b017f4f125909
Reza <reza.daneshvar@majority.com>
2025-11-14 17:30:47 +0100

    Add minimal code to demo the issue

--- animation-issue-demo/ViewController.swift
+++ animation-issue-demo/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let spinningImageView = SpinningImageView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        spinningImageView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(spinningImageView)
+
+        NSLayoutConstraint.activate([
+            spinningImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            spinningImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
+        spinningImageView.start()
+    }
 }
 
+class SpinningImageView: UIImageView {
+    init() {
+        super.init(frame: .zero)
+
+        image = .loremIpsum
+        contentMode = .scaleAspectFit
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func start() {
+        UIView.transition(with: self, duration: 4, options: [.transitionFlipFromRight], animations: nil)
+    }
+}
